7015dc36742ebf40f1625dbbcd6bf1dcace4be41
Anliat MOHAMED <anliatmohamed@gmail.com>
2024-08-22 22:17:14 +0200

    create schema.sql and fix application.properties

---
+++ eventsAppBackend/src/main/resources/schema.sql
@@ -0,0 +1,76 @@
+DROP DATABASE IF EXISTS events_db;
+
+CREATE DATABASE IF NOT EXISTS events_db;
+
+USE events_db;
+
+-- Create the users table
+CREATE TABLE users (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    role ENUM('user', 'admin') NOT NULL
+);
+
+-- Create the events table
+CREATE TABLE events (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    description TEXT NOT NULL,
+    date DATE NOT NULL,  -- format = YYYY-MM-DD
+    time TIME NOT NULL,  -- format = HH:MM:SS
+    logo VARCHAR(255) NULL,  -- Chemin ou URL du logo qui peut être null
+    capacity INT,
+    created_by INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    title VARCHAR(100),
+    location VARCHAR(255),
+    FOREIGN KEY (created_by) REFERENCES users(id)
+);
+
+-- Create the participants table
+CREATE TABLE participants (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    invited_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    responded_at TIMESTAMP DEFAULT NULL,  -- peut être NULL si non répondu
+    status ENUM('invited', 'accepted', 'declined', 'canceled') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
+
+-- Create the notifications table
+CREATE TABLE notifications (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    message TEXT NOT NULL,
+    is_read BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    type ENUM('invitation', 'reminder', 'update') NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
+
+-- Create the comments table
+CREATE TABLE comments (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    user_id INT NOT NULL,
+    event_id INT NOT NULL,
+    content TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    rating INT CHECK (rating BETWEEN 1 AND 5),  -- Note entre 1 et 5
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (event_id) REFERENCES events(id)
+);
+
+
+--drop database events_db_test;
+--create database if not exists events_db_test;
