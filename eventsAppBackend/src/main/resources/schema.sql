2731d2048870a0b82efe440bffb34c3f72d348bc
Rosaline <rosaline.kuy@gmail.com>
2024-08-22 22:28:10 +0200

    update db name

--- eventsAppBackend/src/main/resources/schema.sql
+++ eventsAppBackend/src/main/resources/schema.sql
@@ -5,8 +5,8 @@ CREATE DATABASE IF NOT EXISTS events_db;
 USE events_db;
 
 -- Create the users table
-CREATE TABLE users (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE `user` (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     username VARCHAR(50) NOT NULL,
@@ -18,11 +18,11 @@ CREATE TABLE users (
 );
 
 -- Create the events table
-CREATE TABLE events (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE event (
+    event_id INT PRIMARY KEY AUTO_INCREMENT,
     description TEXT NOT NULL,
-    date DATE NOT NULL,  -- format = YYYY-MM-DD
-    time TIME NOT NULL,  -- format = HH:MM:SS
+    `date` DATE NOT NULL,  -- format = YYYY-MM-DD
+    `time` TIME NOT NULL,  -- format = HH:MM:SS
     logo VARCHAR(255) NULL,  -- Chemin ou URL du logo qui peut être null
     capacity INT,
     created_by INT NOT NULL,
@@ -30,45 +30,45 @@ CREATE TABLE events (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     title VARCHAR(100),
     location VARCHAR(255),
-    FOREIGN KEY (created_by) REFERENCES users(id)
+    FOREIGN KEY (created_by) REFERENCES `user`(user_id)
 );
 
 -- Create the participants table
-CREATE TABLE participants (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE participant (
+    participant_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     event_id INT NOT NULL,
     invited_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     responded_at TIMESTAMP DEFAULT NULL,  -- peut être NULL si non répondu
     status ENUM('invited', 'accepted', 'declined', 'canceled') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (event_id) REFERENCES event(event_id)
 );
 
 -- Create the notifications table
-CREATE TABLE notifications (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE notification (
+    notification_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     event_id INT NOT NULL,
     message TEXT NOT NULL,
     is_read BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     type ENUM('invitation', 'reminder', 'update') NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (event_id) REFERENCES event(event_id)
 );
 
 -- Create the comments table
-CREATE TABLE comments (
-    id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE comment (
+    comment_id INT PRIMARY KEY AUTO_INCREMENT,
     user_id INT NOT NULL,
     event_id INT NOT NULL,
     content TEXT NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     rating INT CHECK (rating BETWEEN 1 AND 5),  -- Note entre 1 et 5
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (event_id) REFERENCES events(id)
+    FOREIGN KEY (user_id) REFERENCES `user`(user_id),
+    FOREIGN KEY (event_id) REFERENCES event(event_id)
 );
 
 
