dee9fcba5586234a2058891eb1732385fde74c0b
Rosaline <rosaline.kuy@gmail.com>
2024-08-22 18:01:10 +0200

    backend java spring boot project created

---
+++ eventsAppBackend/build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.3.2"
+	id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.demo"
+version = "0.0.1-SNAPSHOT"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(21)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("com.mysql:mysql-connector-j")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.testcontainers:junit-jupiter")
+	testImplementation("org.testcontainers:mysql")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ eventsAppBackend/src/main/java/com/demo/eventsAppBackend/EventsAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.demo.eventsAppBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventsAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventsAppBackendApplication.class, args);
+	}
+
+}
