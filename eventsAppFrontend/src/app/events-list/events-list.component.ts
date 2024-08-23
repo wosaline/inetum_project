025ddf65108c8b36f0dad60c8a032564accec2f1
hatem-marzougui <hatem.adresse@gmail.com>
2024-08-23 21:56:46 +0200

    get real events data from db and prepare spinner on loading

--- eventsAppFrontend/src/app/events-list/events-list.component.ts
+++ eventsAppFrontend/src/app/events-list/events-list.component.ts
@@ -2,133 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { EventCardComponent } from '../event-card/event-card.component';
 import { CommonModule } from '@angular/common';
 import { HttpProviderService } from '../../services/http-provider.service';
+import { SpinnerComponent } from '../spinner/spinner.component';
 
 @Component({
   selector: 'app-events-list',
   standalone: true,
-  imports: [EventCardComponent, CommonModule],
+  imports: [EventCardComponent, SpinnerComponent, CommonModule],
   templateUrl: './events-list.component.html',
   styleUrl: './events-list.component.css',
 })
 export class EventsListComponent implements OnInit {
-  eventsList: any[] = [
-    {
-      title:
-        'Long event Title For myAPP HHHH flgkdfjgvolbjdflgvbjdlmfjgvbdlmfjgblmdfjgmldfjgbdmfjgbdfmgbjdfmlgbjfdlm',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-    {
-      title: 'Long event Title For myAPP HHHH',
-      description:
-        'Un événement fait partie intégrante de la stratégie de communication d’une entreprise. Il peut s’agir d’une manifestation pour remercier ses collaborateurs et ses clients, informer d’un changement, célébrer une réussite… Dans tous les cas, un événement sert à faire passer un message et à promouvoir une entreprise. Une présentation écrite permet d’annoncer un événement grand public',
-      date: '11/08/2024',
-      location: 'Paris',
-      time: '10h',
-      capacity: 140,
-    },
-  ];
-
+  eventsList: any[] = [];
+  loading: boolean = true;
   constructor(private httpProviderService: HttpProviderService) {}
   ngOnInit(): void {
-    // remove comments when API  it is ready
-    //  this.loadCompartiments();
+    this.loadCompartiments();
   }
 
-  // loadCompartiments() {
-  //   this.httpProviderService.getAllEvents().subscribe(
-  //     (res) => {
-  //       this.eventsList = res.body;
-  //       console.log('Compartiments:', this.eventsList);
-  //     },
-  //     (error) => {
-  //       console.error('Error fetching compartiments:', error);
-  //     }
-  //   );
-  // }
+  loadCompartiments() {
+    this.httpProviderService.getAllEvents().subscribe(
+      (res) => {
+        this.eventsList = res.body || [];
+        console.log('Events:', this.eventsList);
+        this.loading = false;
+      },
+      (error) => {
+        console.error('Error fetching events:', error);
+        this.loading = false;
+      }
+    );
+  }
 }
