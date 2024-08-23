cf6cc5978526906f7fbbc01306343bcd9f4c7675
Rosaline <rosaline.kuy@gmail.com>
2024-08-23 23:06:07 +0200

    add crossorigins fix

--- eventsAppBackend/src/main/java/com/demo/eventsAppBackend/controller/EventController.java
+++ eventsAppBackend/src/main/java/com/demo/eventsAppBackend/controller/EventController.java
@@ -13,6 +13,7 @@ import java.time.LocalTime;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class EventController {
     private final EventService eventService;
 
