ee1e2e8c97371abf8c3c741a4cc823a65f3beb3f
Rosaline <rosaline.kuy@gmail.com>
2024-08-24 13:41:12 +0200

    correct form size + routing

--- eventsAppFrontend/src/app/app.routes.ts
+++ eventsAppFrontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponentComponent } from './home-component/home-component.component';
+import { RegisterComponent } from './register/register.component';
 
 //on va definir les routes
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponentComponent },
+  { path: 'register', component: RegisterComponent},
 ];
