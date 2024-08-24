1b23e0df266b40d4360328878ae7ccd286fb07b6
Anliat MOHAMED <anliatmohamed@gmail.com>
2024-08-24 16:29:12 +0200

    update getAllEventsByUserId to get all the events invited/accepted for an userID

--- eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
+++ eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
@@ -1,12 +1,13 @@
 package com.demo.eventsAppBackend.repository;
 
 import com.demo.eventsAppBackend.model.Participant;
+import com.demo.eventsAppBackend.model.ParticipantStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 
-    List<Participant> findAll();
+    List<Participant> findAllByUserIdAndStatusIn(int userId, List<ParticipantStatus> statuses);
 
 }
