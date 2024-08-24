2127af8e3512c404421bb045a817e0c6fc702fe7
Anliat MOHAMED <anliatmohamed@gmail.com>
2024-08-24 21:54:07 +0200

    upadate participant = update status, create response

--- eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
+++ eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 
+    Participant findById(int id);
     List<Participant> findAllByUserIdAndStatusIn(int userId, List<ParticipantStatus> statuses);
 
 }
