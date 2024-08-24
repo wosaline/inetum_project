813b8ed4fde3d3844772449a17f55ee3d65aaaa3
Rosaline <rosaline.kuy@gmail.com>
2024-08-24 15:07:22 +0200

    add form control

--- eventsAppFrontend/src/app/register/register.component.ts
+++ eventsAppFrontend/src/app/register/register.component.ts
@@ -28,20 +28,12 @@ export class RegisterComponent implements OnInit {
         username: ['', Validators.required],
         email: ['', [Validators.required, Validators.email]],
         password: ['', [Validators.required, Validators.minLength(6)]],
-        confirmPassword: ['', Validators.required],
         firstName: ['', Validators.required],
         lastName: ['', Validators.required],
-        terms: [false, Validators.requiredTrue],
       },
-      { validator: this.passwordMatchValidator }
     );
   }
 
-  passwordMatchValidator(form: FormGroup) {
-    return form.get('password')?.value === form.get('confirmPassword')?.value
-      ? null
-      : { mismatch: true };
-  }
 
   onSubmit() {
     if (this.registerForm.valid) {
@@ -52,6 +44,8 @@ export class RegisterComponent implements OnInit {
       // Redirect to another page after successful registration
 
       this.router.navigate(['/login']); //Replace '/login' with the route you want to redirect the user to
+    }else{
+      console.log("Form incomplete?");
     }
   }
 }
