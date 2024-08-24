cb0c44269fc9d9c5158eb0be1eab1971bb94944b
Rosaline <rosaline.kuy@gmail.com>
2024-08-24 17:32:54 +0200

    remove unused code

--- eventsAppFrontend/src/services/web-api.service.ts
+++ eventsAppFrontend/src/services/web-api.service.ts
@@ -9,18 +9,6 @@ import { User } from '../interfaces/user';
 export class WebAPIService {
   constructor(private httpClient: HttpClient) {}
 
-  public convertUserToFormData(user: User): FormData {
-    const formData = new FormData();
-    console.log(user);
-    formData.append('username', user.username);
-    formData.append('email', user.email);
-    formData.append('passwordHash', user.passwordHash);
-    formData.append('firstName', user.firstName);
-    formData.append('lastName', user.lastName);
-    formData.append('role', user.role);
-    return formData;
-  }
-
   get(url: string): Observable<any> {
     const httpOptions = {
       headers: new HttpHeaders({
