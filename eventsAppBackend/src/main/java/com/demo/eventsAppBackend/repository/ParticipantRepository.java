63fb05b0040df07cf710cccaf1fb1dd485e0226d
hatem-marzougui <hatem.adresse@gmail.com>
2024-08-25 11:04:14 +0200

    participant with accepted status can only comment

--- eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
+++ eventsAppBackend/src/main/java/com/demo/eventsAppBackend/repository/ParticipantRepository.java
@@ -1,12 +1,21 @@
 package com.demo.eventsAppBackend.repository;
 
+import com.demo.eventsAppBackend.model.Event;
 import com.demo.eventsAppBackend.model.Participant;
+import com.demo.eventsAppBackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ParticipantRepository extends JpaRepository<Participant, Integer> {
 
     List<Participant> findAll();
+
+    @Query("SELECT p FROM Participant p WHERE p.user.id = :userId AND p.event.id = :eventId")
+    Participant findByUserIdAndEventId(@Param("userId") int userId, @Param("eventId") int eventId);
+
+
 
 }
