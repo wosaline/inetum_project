1edc9abe9a8bc3cf77159cfe1b5db73b32727003
gbiyoghe27 <gcbiyoghe@gmail.com>
2024-08-28 20:22:54 +0200

    user-profile

--- eventsAppFrontend/src/app/app.routes.ts
+++ eventsAppFrontend/src/app/app.routes.ts
@@ -14,6 +14,7 @@ export const routes: Routes = [
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
   { path: 'event-page', component: EventPageComponent },
+  { path: 'user-profile', component: UserProfileComponent },
 ];
 
 export class AppRoutingModule {}
--- eventsAppFrontend/src/app/user-profile/user-profile.component.ts
+++ eventsAppFrontend/src/app/user-profile/user-profile.component.ts
@@ -7,11 +7,12 @@ import {
 } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [ReactiveFormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule, NavbarComponent],
   templateUrl: './user-profile.component.html',
   styleUrls: ['./user-profile.component.css'],
 })
@@ -51,7 +52,7 @@ export class UserProfileComponent implements OnInit {
       password: [{ value: '********', disabled: true }],
       firstName: [this.user.firstName, Validators.required],
       lastName: [this.user.lastName, Validators.required],
-      role: [{ value: this.user.role, disabled: true }],
+      role: [{ value: this.user.role, disabled: this.user.role !== 'admin' }],
     });
   }
 
@@ -64,11 +65,20 @@ export class UserProfileComponent implements OnInit {
         this.profileForm.get('username')?.enable();
         this.profileForm.get('email')?.enable();
       }
+      this.setRoleFieldState(); // Vérifiez l'état du champ `role` lors de l'édition
     } else {
       this.profileForm.get('username')?.disable();
       this.profileForm.get('email')?.disable();
       this.profileForm.get('firstName')?.disable();
       this.profileForm.get('lastName')?.disable();
+    }
+  }
+
+  setRoleFieldState() {
+    if (this.user.role === 'admin') {
+      this.profileForm.get('role')?.enable();
+    } else {
+      this.profileForm.get('role')?.disable();
     }
   }
 
