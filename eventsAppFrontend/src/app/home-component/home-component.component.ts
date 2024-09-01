cdfeb46998be89b47f25ce4ff8da3fdd175ad1b3
Rosaline <rosaline.kuy@gmail.com>
2024-09-01 15:43:41 +0200

    create backend + frontend for private event checkbox

--- eventsAppFrontend/src/app/home-component/home-component.component.ts
+++ eventsAppFrontend/src/app/home-component/home-component.component.ts
@@ -2,34 +2,55 @@ import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../navbar/navbar.component';
 import { EventsListComponent } from '../events-list/events-list.component';
 import { HttpProviderService } from '../../services/http-provider.service';
+import { AuthService } from '../../services/auth.service';
+import { FormsModule, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [NavbarComponent, EventsListComponent],
+  imports: [NavbarComponent, EventsListComponent, FormsModule],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css',
 })
 export class HomeComponentComponent implements OnInit {
   eventsList: any[] = [];
   loading: boolean = true;
+  isLoggedIn: boolean = false;
+  showPrivateEvents: boolean = true;
 
-  constructor(private httpProviderService: HttpProviderService) {}
+  constructor(private httpProviderService: HttpProviderService, private authService: AuthService) {}
   ngOnInit(): void {
+    this.isLoggedIn = this.authService.isAuthenticated();
     this.loadEvents();
   }
 
   loadEvents() {
-    this.httpProviderService.getAllEvents().subscribe(
-      (res) => {
-        this.eventsList = res.body || [];
-        console.log('Events:', this.eventsList);
-        this.loading = false;
-      },
-      (error) => {
-        console.error('Error fetching events:', error);
-        this.loading = false;
-      }
-    );
+    this.loading = true;
+    
+    if (this.isLoggedIn && this.showPrivateEvents) {
+      this.httpProviderService.getAllEvents().subscribe(
+        (res) => {
+          this.eventsList = res.body || [];
+          console.log('Events:', this.eventsList);
+          this.loading = false;
+        },
+        (error) => {
+          console.error('Error fetching events:', error);
+          this.loading = false;
+        }
+      );
+    } else {
+      this.httpProviderService.getAllPublicEvents().subscribe(
+        (res) => {
+          this.eventsList = res.body || [];
+          console.log('Events:', this.eventsList);
+          this.loading = false;
+        },
+        (error) => {
+          console.error('Error fetching events:', error);
+          this.loading = false;
+        }
+      );
+    }
   }
 }
