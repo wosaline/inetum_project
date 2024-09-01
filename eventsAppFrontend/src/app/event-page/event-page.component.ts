89bd83d92425ddfe2b5759c38f85615f9240832b
Rosaline <rosaline.kuy@gmail.com>
2024-09-01 17:38:22 +0200

    fix css

--- eventsAppFrontend/src/app/event-page/event-page.component.ts
+++ eventsAppFrontend/src/app/event-page/event-page.component.ts
@@ -130,7 +130,6 @@ export class EventPageComponent implements OnInit {
   onSubmit(): void {
     if (this.eventForm.valid) {
       const event: Event = this.eventForm.value;
-      // TODO ONCE LOGGING IS ON
       event.createdBy = this.userId!;
       if (this.isEditing) {
         event.id = this.currentEventId;
@@ -170,10 +169,10 @@ export class EventPageComponent implements OnInit {
   }
 
   reset(): void {
-    // this.isEditing = false;
-    // this.currentEventId = undefined;
-    window.location.reload();
-    // this.eventForm.reset();
+    this.isEditing = false;
+    this.currentEventId = undefined;
+    // window.location.reload();
+    this.eventForm.reset();
   }
 
   toggleEventForm(): void {
