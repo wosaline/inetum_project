ddc8567488c41b8dd50b2b2f6631c80bf01be646
wosaline <43705128+wosaline@users.noreply.github.com>
2024-09-01 14:53:30 +0200

    Update Comment.ts

--- eventsAppFrontend/src/interfaces/Comment.ts
+++ eventsAppFrontend/src/interfaces/Comment.ts
@@ -1,11 +1,1 @@
-import { User } from './user';
 
-export interface Comment {
-  id?: number;
-  event: Event;
-  user: User;
-  content: String;
-  createdAt: Date;
-  updateddAt: Date;
-  rating: number;
-}
